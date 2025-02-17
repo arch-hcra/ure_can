32acb6fe7f5f63e5f04d9199f4514a97bf27bca3
arch-hcra <zxxCAPRICExxz@gmail.com>
2025-02-17 13:56:53 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -12,6 +12,7 @@ class TestApp(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(5, 5), 0)
+        self.assertEqual(subtract(10**6, 10**5), 900000)
 
 if __name__ == "__main__":
     unittest.main()
